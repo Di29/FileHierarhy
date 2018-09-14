d86c83523259b831b8e9623a7acf1331b9cf6928
Уринбасаров Диас Нуржанулы <@corp.itstep>
2018-09-14 15:35:23 +0600

    start

---
+++ FileHierarhy/File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHierarhy
+{
+    public class File
+    {
+        public string FileName { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string FilePath { get; set; }
+
+
+
+    }
+}
---
+++ FileHierarhy/Folder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHierarhy
+{
+    public class Folder
+    {
+        public string FolderName { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string FolderPath { get; set; }
+
+        public Folder()
+        {
+
+        }
+    }
+}
